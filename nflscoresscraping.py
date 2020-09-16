c3e2d075d212dda119aa0d0a9eca404f30a230d2
Dariush <11928992+nebulousman@users.noreply.github.com>
2020-09-16 14:45:58 -0400

    Update nflscoresscraping.py

--- nflscoresscraping.py
+++ nflscoresscraping.py
@@ -10,12 +10,23 @@ Created on Tue Sep 1 14:02:05 2020
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 
 # get week NFL results
 
 # target URL pro-football-reference
 url = "https://www.pro-football-reference.com/boxscores/"
 
+data = pd.read_html(url)
+scores = pd.DataFrame(np.concatenate(data))
+
+# remove stat rows
+statrows = ['PassYds','RushYds','RecYds']
+scores = scores[~scores[0].isin(statrows)]
+
+
+## ignore below ##
+## above code might be easier way to accomplish the goal of retrieving results in a dataframe
 # headers
 headers = {
     'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
